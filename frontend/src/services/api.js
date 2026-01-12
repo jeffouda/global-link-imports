dc0fcdfc5a82c4ab668054168aa7ebdb8092d55d
Jeff Ouda <jeffoudahabakuk@gmail.com>
2026-01-12 03:56:43 +0300

    Add services directory

---
+++ frontend/src/services/api.js
@@ -0,0 +1,24 @@
+import axios from 'axios';
+
+// 1. Create a central Axios instance
+const api = axios.create({
+    baseURL: 'http://127.0.0.1:5000', // Connects to your Flask Backend
+    headers: {
+        'Content-Type': 'application/json',
+    }
+});
+
+// 2. Add an "Interceptor" to attach tokens automatically
+// (This saves you from manually adding the token to every request later)
+api.interceptors.request.use(
+    (config) => {
+        const token = localStorage.getItem('token');
+        if (token) {
+            config.headers.Authorization = `Bearer ${token}`;
+        }
+        return config;
+    },
+    (error) => Promise.reject(error)
+);
+
+export default api;
