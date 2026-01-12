dbf09d083a401f41d03a2a03d31829f7d09c8661
Jeff Ouda <jeffoudahabakuk@gmail.com>
2026-01-12 04:00:32 +0300

    Add basic structure to DashboardPage

---
+++ frontend/src/pages/DashboardPage.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const DashboardPage = () => {
+  return (
+    <div>
+      <h1>Dashboard</h1>
+      <p>Overview of your shipments and inventory.</p>
+    </div>
+  );
+};
+
+export default DashboardPage;
