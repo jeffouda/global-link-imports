6ded72e203bfac78c67b038c37543ee94160aecc
Jeff Ouda <jeffoudahabakuk@gmail.com>
2026-01-12 04:01:35 +0300

    Add basic structure to ShipmentListPage

---
+++ frontend/src/pages/ShipmentListPage.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const ShipmentListPage = () => {
+  return (
+    <div>
+      <h1>Shipments</h1>
+      <p>List of all shipments.</p>
+    </div>
+  );
+};
+
+export default ShipmentListPage;
