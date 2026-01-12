2e36f072bbf5f5cc660f164ffa617839fcae690d
Jeff Ouda <jeffoudahabakuk@gmail.com>
2026-01-12 04:02:13 +0300

    Add basic structure to ShipmentDetailsPage

---
+++ frontend/src/pages/ShipmentDetailsPage.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const ShipmentDetailsPage = () => {
+  return (
+    <div>
+      <h1>Shipment Details</h1>
+      <p>Details of the selected shipment.</p>
+    </div>
+  );
+};
+
+export default ShipmentDetailsPage;
