a46f300f91ad9d0640a3a939790ac21b64703b1c
Jeff Ouda <jeffoudahabakuk@gmail.com>
2026-01-12 04:01:47 +0300

    Add basic structure to TrackingPage

---
+++ frontend/src/pages/TrackingPage.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const TrackingPage = () => {
+  return (
+    <div>
+      <h1>Track Shipment</h1>
+      <p>Enter tracking number to track your shipment.</p>
+    </div>
+  );
+};
+
+export default TrackingPage;
