1ac53c5bbec0f0c741e774aa85b6ede891a2ac5a
Jeff Ouda <jeffoudahabakuk@gmail.com>
2026-01-12 04:00:16 +0300

    Add basic structure to HomePage

---
+++ frontend/src/pages/HomePage.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const HomePage = () => {
+  return (
+    <div>
+      <h1>Welcome to GlobaLink</h1>
+      <p>Your global shipping solution.</p>
+    </div>
+  );
+};
+
+export default HomePage;
