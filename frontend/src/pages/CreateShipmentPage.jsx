180d57c58cf8a40c73d0b3223b99a43b3fe726fd
Jeff Ouda <jeffoudahabakuk@gmail.com>
2026-01-12 04:01:58 +0300

    Add basic structure to CreateShipmentPage

---
+++ frontend/src/pages/CreateShipmentPage.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const CreateShipmentPage = () => {
+  return (
+    <div>
+      <h1>Create Shipment</h1>
+      <p>Fill in the details to create a new shipment.</p>
+    </div>
+  );
+};
+
+export default CreateShipmentPage;
