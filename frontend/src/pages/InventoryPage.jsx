336b2a2c90522a085f17bf88e2b1789f8a296166
Jeff Ouda <jeffoudahabakuk@gmail.com>
2026-01-12 04:01:19 +0300

    Add basic structure to InventoryPage

---
+++ frontend/src/pages/InventoryPage.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const InventoryPage = () => {
+  return (
+    <div>
+      <h1>Inventory</h1>
+      <p>Manage your products.</p>
+    </div>
+  );
+};
+
+export default InventoryPage;
