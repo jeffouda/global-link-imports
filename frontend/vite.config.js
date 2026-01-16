687f4b7799edf40f20321a31dedbb22e312d7474
Michelle Nyagaka <michellenyagaka@gmail.com>
2026-01-16 09:28:32 +0300

    Update package dependencies and clean up configuration file

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,6 +1,8 @@
-export default {
-  plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
-  },
-};
+// vite.config.js
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react], 
+});
